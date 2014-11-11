f9514e2b9d8d46eb0bbf1756d890c58680d32ec8
andycanderson <chen.a.andy@gmail.com>
2014-11-11 15:17:18 -0800

    updating background formatting to cover entire screen

--- app/models/mood.rb
+++ app/models/mood.rb
@@ -19,29 +19,30 @@ class Mood
   has_and_belongs_to_many :places
 
   def searchYelp(current_user)
-  params = {
-    category_filter: self.category.path,
-    # limit: 1,
-    radius_filter: current_user.radius,
-    sort: self.sort,
-    term: self.keywords.join('+')
-  }
 
-if current_user.latitude > 0 && current_user.traveling
-# pop up modal
-# Iffy noticed you're not searching from cityname. Would you like to search in newcityname instead? You could also update your location in your account profile
-  coordinates = {
-    latitude: current_user.latitude,
-    longitude: current_user.longitude
-  }
-else
-  coordinates = {
-    latitude: current_user.zip.to_s.to_lat,
-    longitude: current_user.zip.to_s.to_lon
-  }
-end
-  # current_user.places << @mood.searchYelp(current_user).businesses[0].id
-  @yelp_results ||= Yelp.client.search_by_coordinates(coordinates, params)
+    params = {
+      category_filter: self.category.path,
+      # limit: 1,
+      radius_filter: current_user.radius,
+      sort: self.sort,
+      term: self.keywords.join('+')
+    }
+
+    if current_user.latitude > 0 && current_user.traveling
+    # pop up modal
+    # Iffy noticed you're not searching from cityname. Would you like to search in newcityname instead? You could also update your location in your account profile
+      coordinates = {
+        latitude: current_user.latitude,
+        longitude: current_user.longitude
+      }
+    else
+      coordinates = {
+        latitude: current_user.zip.to_s.to_lat,
+        longitude: current_user.zip.to_s.to_lon
+      }
+    end
+      # current_user.places << @mood.searchYelp(current_user).businesses[0].id
+    @yelp_results ||= Yelp.client.search_by_coordinates(coordinates, params)
 
   end
 end
